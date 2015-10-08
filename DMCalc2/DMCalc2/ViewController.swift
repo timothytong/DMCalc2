5dc76e3e198da387889df1a7df9396b961b8f2e4
timothytong <timothy.tong@hotmail.ca>
2015-10-07 22:43:22 -0700

    Restarted project

---
+++ DMCalc2/DMCalc2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DMCalc2
+//
+//  Created by Timothy Tong on 2015-10-07.
+//  Copyright © 2015 Timothy Tong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
